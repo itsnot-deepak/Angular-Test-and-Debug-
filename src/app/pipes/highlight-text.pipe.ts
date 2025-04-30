26a13bab788f06b209b7def4b95ac13cd5e74381
Deepak <112324970+itsnot-deepak@users.noreply.github.com>
2025-04-30 11:24:06 +0000

    messages

--- src/app/pipes/highlight-text.pipe.ts
+++ src/app/pipes/highlight-text.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'highlightText'
+    name: 'highlightText' // should be named camel case  
 })
 export class HighlightTextPipe implements PipeTransform {
 
@@ -10,8 +10,11 @@ export class HighlightTextPipe implements PipeTransform {
             return value;
         }
 
-        const search = new RegExp(filter, 'ig');
+        const search = new RegExp(filter, 'ig'); // here we are making regex exp and then giving the filter as the pattern also we are giving i- which is to ignore the case sensitivity and then we give g which is the global 
+        // by default the regex matches just the first occurence but this makes it match all the occurrence throught the system 
 
+        // here we are using the replace function to replace the matched values 
+        // so we send the matched instance in the match variable and then we wrap/return an span tag around it which will use the highlight-text class to highlight the matched value 
         return value.replace(search, (match) => {
             return `<span class="highlight-text">${match}</span>`;
         });
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -3,7 +3,8 @@ import { User } from '../user/user';
 import { UserListService } from './user-list.service';
 
 import { WebStorageService } from '../services/web-storage.service';
-// i do not understand what is happening in this project , the person is using an mock api server which intercepts every request the doumentation is mostly related to the videos 
+// i do not understand what is happening in this project , the person is using an mock api server which intercepts every request the doumentation is mostly related to the videos
+//here we are creating an local storage to show the name of the users from the filter even when we reload the page  
 @Component({
     selector: 'app-user-list',
     templateUrl: './user-list.component.html',
@@ -14,7 +15,7 @@ export class UserListComponent implements OnInit {
 
   constructor(
     private userListService: UserListService,
-    private webStorageService: WebStorageService
+    private webStorageService: WebStorageService // DI of the webstorageservice 
   ) { }
 
   public async ngOnInit(): Promise<void> {
